eac2bda963618eda829135c190b98455a3d947ad
Parami <paramiashinsana@gmail.com>
2024-08-25 09:04:49 +0530

    Update User

--- src/main/java/org/example2/testdemo/service/UserService.java
+++ src/main/java/org/example2/testdemo/service/UserService.java
@@ -1,4 +1,12 @@
 package org.example2.testdemo.service;
 
+import org.example2.testdemo.dto.UserDTO;
+
+import java.util.List;
+
 public interface UserService {
+    UserDTO saveUser(UserDTO userDTO);
+    List<UserDTO> getAllUser();
+    void deleteUser(String id);
+    void updateUser(String id, UserDTO userDTO);
 }
