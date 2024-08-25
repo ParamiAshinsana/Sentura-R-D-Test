ab8b73e8dd621369b781d7a4f3879744bb6ad97d
Parami <paramiashinsana@gmail.com>
2024-08-25 09:12:11 +0530

    Update UserController and UserServiceIMPL

--- src/main/java/org/example2/testdemo/controller/UserController.java
+++ src/main/java/org/example2/testdemo/controller/UserController.java
@@ -1,6 +1,10 @@
 package org.example2.testdemo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example2.testdemo.dto.UserDTO;
+import org.example2.testdemo.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/")
 @RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
+    @PostMapping(value = "/")
+    public UserDTO saveUsers(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
 }
--- src/main/java/org/example2/testdemo/service/impl/UserServiceIMPL.java
+++ src/main/java/org/example2/testdemo/service/impl/UserServiceIMPL.java
@@ -1,10 +1,16 @@
 package org.example2.testdemo.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.example2.testdemo.dto.UserDTO;
 import org.example2.testdemo.service.UserService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserServiceIMPL implements UserService {
     @Override
     public UserDTO saveUser(UserDTO userDTO) {
