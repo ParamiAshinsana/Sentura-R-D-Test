55a093c4785bea25dfa51fb41ac77e2db0c312aa
Parami <paramiashinsana@gmail.com>
2024-08-25 09:59:06 +0530

    Update DTO

--- src/main/java/org/example2/testdemo/dto/UserDTO.java
+++ src/main/java/org/example2/testdemo/dto/UserDTO.java
@@ -1,18 +1,23 @@
 package org.example2.testdemo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 public class UserDTO {
     private String uid;
     private String email;
     private String given_name;
     private String middle_name;
+    private String name;
+    private String family_name;
+    private String nickname;
     private String phone_number;
+    private String comment;
+    private String picture;
+    private String directory;
+    private String tags;
+    private boolean is_suspended;
+    private boolean is_bot;
 }
