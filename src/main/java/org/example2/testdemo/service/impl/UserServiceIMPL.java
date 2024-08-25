f399781014ec17ae3fbc605d86dde70d15fe1fa9
Parami <paramiashinsana@gmail.com>
2024-08-25 09:05:31 +0530

    Update UserServiceIMPL

--- src/main/java/org/example2/testdemo/service/impl/UserServiceIMPL.java
+++ src/main/java/org/example2/testdemo/service/impl/UserServiceIMPL.java
@@ -1,4 +1,28 @@
 package org.example2.testdemo.service.impl;
 
-public class UserServiceIMPL {
+import org.example2.testdemo.dto.UserDTO;
+import org.example2.testdemo.service.UserService;
+
+import java.util.List;
+
+public class UserServiceIMPL implements UserService {
+    @Override
+    public UserDTO saveUser(UserDTO userDTO) {
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUser() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String id) {
+
+    }
+
+    @Override
+    public void updateUser(String id, UserDTO userDTO) {
+
+    }
 }
